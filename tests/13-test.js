6b4cb42f78e007602972750d8a0bd5370a414e1f
urain39 <urain39@qq.com>
2020-05-12 20:35:39 +0800

    test: add test for #35

---
+++ tests/13-test.js
@@ -0,0 +1,12 @@
+// INFO: test for #808e99f55f277ee2d04e736195a897b3ae60225a
+
+var IJ2TPL = require('../dist/ij2tpl.min');
+
+let source = `{a}{?a}{a}{/a}`;
+
+let tpl = IJ2TPL.parse(source);
+
+if (tpl.render({a: { a: 0}}) === `[object Object]0`)
+	console.log(`${__filename}: PASS`);
+else
+	console.log(`${__filename}: FAIL`);
